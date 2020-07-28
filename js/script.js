d86d4cc809b42d186ae0f6b2e54a60f97e0fbf63
Aftab Husain <aftabhusain687@gmail.com>
2020-07-29 01:22:23 +0530

    add files

--- js/script.js
+++ js/script.js
@@ -1,7 +1,49 @@
+function demo(){
+	
+	alert("Hello world");
+	
+	
+}
 
+'''
+function ONLOAD(){
+	alert("onload event call hua");
+}
+
+
+function CLICK(){
+	document.write(new Date().getMinutes());
+}
+
+
+function HOVER(){
+	alert("hover event call hua");
+}
+
+function ONMOUSEOUT(){
+	alert("on mouse out event call hua");
+}
+
+function ONCHANGE(){
+	alert("on change event call hua");
+}
 
 
 function myFunction(){
-	alert("Hello world");
-	document.getElementById("demo").innerHTML="kanpur"
+	var f=document.getElementById("Fname").value;
+	var L=document.getElementById("Lname").value;
+	console.log(f,L);
+	
 }
+
+
+/*//variable declaration 
+var x;
+
+//value assignation 
+
+var x=5;
+
+Gaurav_sharma
+gauravSharma
+GauravSharma*/
