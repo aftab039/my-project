ce02f730b3195f529f053b26ef8e3d1528b6eb87
Aftab Husain <aftabhusain687@gmail.com>
2020-08-04 20:01:56 +0530

    add js files

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 function myFunction(){
     alert("hello world");
    document.getElementById("demo").innerHTML = "kanpur";
+   console.log ("object age is",demo.mob)
 }
+
+
+
+
+
+var demo={name:{name1:"aftab"},
+	age:22,
+	mob:9889440531
+}
+
+
+
+demo.name.name1
