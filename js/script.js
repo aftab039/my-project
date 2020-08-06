40588eb1849a48aa561c858cf9f9f30627c34456
Aftab Husain <aftabhusain687@gmail.com>
2020-08-06 09:10:55 +0530

    add js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 function myFunction(){
-    alert("hello world");
    document.getElementById("demo").innerHTML = "kanpur";
-   console.log ("object age is",demo.mob)
+   console.log ("city value" ,city[3])
 }
 
 
@@ -16,3 +15,14 @@ var demo={name:{name1:"aftab"},
 
 
 demo.name.name1
+
+
+var person ={
+	name:"gaurav",
+	age:29,
+	address:{state:"uttar pradesh",city:"kanpur"},
+	mob:9889440531
+};
+
+var city =["kanpur","sultanpur","lucknow","jaipur","mumbai", "ahmedabad"]
+
