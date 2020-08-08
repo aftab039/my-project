138689952c657ab161d6352df3bdd5eace6d2a9d
Aftab Husain <aftabhusain687@gmail.com>
2020-08-09 01:13:38 +0530

    add codes

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ function myFunction(){
 	// document.write(city);
 
 
-
-	 document.write(temp);
+document.write(person);
+	//  document.write(temp);
 	document.write("--my old array-----",city);
 
 
-   document.getElementById("demo").innerHTML = "kanpur";
-   console.log ("city value" ,city[3])
+//    document.getElementById("demo").innerHTML = "kanpur";
+//    console.log ("city value" ,city[3])
 }
 
 
@@ -33,13 +33,21 @@ var person ={
 };
 
 var city =["kanpur","sultanpur","lucknow","jaipur","mumbai", "ahmedabad"]
+var test=["test1","test2","test3","test4"];
 
-city.push("data dalo"); // data add in last into array
-city.pop(); // data remove from last array
-city.shift(); // data remove in first array
-city.unshift("unshift"); // add data starting in array
+city.push(...test); // data add in last into array
 
-var temp=city.splice(1,3);
+// city.pop(); // data remove from last array
+// city.shift(); // data remove in first array
+// city.unshift("unshift"); // add data starting in array
+
+// var temp=city.splice(1,3);
 
 
 
+ person.gender="male";
+
+ // errors 
+ undefined
+ null
+ is not define 
